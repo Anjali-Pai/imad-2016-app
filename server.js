c90121083ea79aeb2d7f7afa9c131c3642b1d3d2
Angel <anjalipai8296@gmail.com>
2016-09-25 16:24:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,94 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articelOne = {
+    title:'Article one|Anjali Pai',
+    heading:'Article One',
+    date:'Sept 5, 2016',
+    content: `<p>
+       This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the
+       content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+       This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is
+       content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+      </p>
+      <p>
+       This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the
+       content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+       This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is
+       content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+      </p>
+      <p>
+       This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the
+       content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+       This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is
+       content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+      </p>`
+    
+};
+
+function createTemplate(data){
+    var title =data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content =data.content;
+    
+    var htmlTemplate =`
+            <!doctype html>
+        <head>
+            <title>
+                Article one | Anajli Pai
+            </title>
+            <meta name="viewport" content="width-device-width, initial-scale-1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+            <style>
+             
+            </style>
+        </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                Article one
+            </h3>
+        
+            <div>
+            sept 23,2016
+            </div>
+            <div>
+              <p>
+               This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the
+               content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+               This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is
+               content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+              </p>
+              <p>
+               This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the
+               content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+               This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is
+               content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+              </p>
+              <p>
+               This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the
+               content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+               This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is
+               content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content
+              </p>
+            </div>
+            </div>
+        
+        
+        </body>
+        </html>
+`;
+}
+
+
+
+
+
 app.get('/article-one', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
