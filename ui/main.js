c14045a17a1007b5e46813a26b84e96118df3408
Angel <anjalipai8296@gmail.com>
2016-10-02 12:42:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,26 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+
+button.onclick=function()
+{
+    //create a request object
+    var request = new XMLHttpRequest();
+    // caputure response and store it in a variable
+    request.onreadystatechange() = function ()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            //TAKE SOME ACTION
+            if (request.status === 200)
+            {
+                    var counter = request.responseText;
+                    var span = document.getElementById('counter');
+                    span.innerHTML = counter.toString();
+            }
+        }
+        //NOT DONE YET
+    };
+   request.open('GET','http://http://anjali-pai.imad.hasura-app.io/counter',true);
+   request.send(null);
+    
+};
